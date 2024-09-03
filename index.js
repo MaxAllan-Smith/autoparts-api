0a7153da8511e35e96850d71ecf45384590703d4
Max-Allan-Smith <max.allan-smith@mit-tech.co.uk>
2024-09-03 10:19:48 +0100

    Updated index.js

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-require('dotenv');
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 app.use(morgan('dev'));
 
-const userRouter = require('./routes/user');
+// MongoDB Connection
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('Connected to MongoDB'))
+.catch(err => console.error('Could not connect to MongoDB:', err));
 
+// Routes
+const userRouter = require('./routes/user');
 app.use('/api', userRouter);
 
-app.listen(PORT,  () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
